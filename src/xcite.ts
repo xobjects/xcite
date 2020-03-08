a004dd21ee9c2c046ca8f94287c5de50594dd385
Lee Kelly <gkelly@Lees-MacBook-Pro.local>
2020-03-08 19:56:19 -0400

    preparing npm

--- src/xcite.ts
+++ src/xcite.ts
@@ -3,10 +3,10 @@ const s_re_js = /\{\{\+([\s\S]+?)\}\}/g;
 const s_re_int = /\{\{\s*(\S+)\s*\}\}/g;
 const s_templates: { [name: string]: (p_data: any) => '' } = {};
 
-export const sablono_get = (p_name: string) => s_templates[p_name];
-export const sablono_run = (p_name: string, p_data?: any) => sablono_get(p_name)(p_data);
+export const xcite_get = (p_name: string) => s_templates[p_name];
+export const xcite_run = (p_name: string, p_data?: any) => xcite_get(p_name)(p_data);
 
-export function sablono_template(p_template: string, p_name?: string) {
+export function xcite_template(p_template: string, p_name?: string) {
 	const v_t = p_template
 		.trim()
 		.replace(/[\s\r\n]+/g, ' ')
@@ -16,7 +16,7 @@ export function sablono_template(p_template: string, p_name?: string) {
 
 	const v_fs = `let o=''; o+= '${v_t}'; return o;`;
 	const v_f0 = new Function('s, p_run', v_fs);
-	const v_f = (p_data?: any) => v_f0(p_data, sablono_run);
+	const v_f = (p_data?: any) => v_f0(p_data, xcite_run);
 
 	if (p_name) {
 		s_templates[p_name] = v_f;
