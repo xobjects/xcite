55bc39545ec47c6a1d2f54ec8e471d9bdab52291
Lee Kelly <Lee.Kelly@uticanational.com>
2020-03-09 18:52:34 -0400

    preparing examples

--- src/xcite.ts
+++ src/xcite.ts
@@ -1,26 +1,26 @@
 const s_re_sub = /\{\{%\s*(\S+)\s+(\S+)\s*\}\}/g;
 const s_re_js = /\{\{\+([\s\S]+?)\}\}/g;
 const s_re_int = /\{\{\s*(\S+)\s*\}\}/g;
-const s_templates: { [name: string]: (p_data: any) => '' } = {};
+const s_templates: { [name: string]: (x: any) => '' } = {};
 
-export const xcite_get = (p_name: string) => s_templates[p_name];
-export const xcite_run = (p_name: string, p_data?: any) => xcite_get(p_name)(p_data);
+export const xcite_get = (name: string) => s_templates[name];
+export const xcite_run = (name: string, x?: any) => xcite_get(name)(x);
 
-export function xcite_template(p_template: string, p_name?: string) {
-	const v_t = p_template
-		.trim()
-		.replace(/[\s\r\n]+/g, ' ')
-		.replace(s_re_sub, (...p_params) => `'; o+=p_run('${p_params[1]}', ${p_params[2]}); o+='`)
-		.replace(s_re_js, (...p_params) => `'; ${p_params[1]}; o+='`)
-		.replace(s_re_int, (...p_params) => `'; o+=${p_params[1]}; o+='`);
+export function xcite_template(template: string, name?: string) {
+    const t = template
+        .trim()
+        .replace(/[\s\r\n]+/g, ' ')
+        .replace(s_re_sub, (...params) => `'; o+=run('${params[1]}', ${params[2]}); o+='`)
+        .replace(s_re_js, (...params) => `'; ${params[1]}; o+='`)
+        .replace(s_re_int, (...params) => `'; o+=${params[1]}; o+='`);
 
-	const v_fs = `let o=''; o+= '${v_t}'; return o;`;
-	const v_f0 = new Function('s, p_run', v_fs);
-	const v_f = (p_data?: any) => v_f0(p_data, xcite_run);
+    const fs = `let o=''; o+= '${t}'; return o;`;
+    const f0 = new Function('x, run', fs);
+    const f = (x?: any) => f0(x, xcite_run);
 
-	if (p_name) {
-		s_templates[p_name] = v_f;
-	}
+    if (name) {
+        s_templates[name] = f;
+    }
 
-	return v_f;
+    return f;
 }
